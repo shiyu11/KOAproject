bb0cfe983acf2f78b55fa9522156c46135486363
sunnyyun521 <2469682513@qq.com>
2018-09-19 21:04:36 +0800

    订单的操作

--- controllers/addressController.js
+++ controllers/addressController.js
@@ -1,5 +1,17 @@
 var addressDAO = require('../model/addressDAO');
 module.exports = {
+    getAddress1:async (ctx, next) => {
+
+        try {
+            //2.调用用户数据访问对象的添加方法
+            let address1 = await addressDAO.getAddress1 ();
+            //3.反馈结果
+            ctx.body = {"code": 200, "message": "ok", data: address1}
+        } catch (err) {
+            ctx.body = {"code": 500, "message": err.toString(), data: []}
+        }
+    },
+
     addAddress: async (ctx, next) => {
         let address = {};
         address.aid =ctx.request.body.aid;
